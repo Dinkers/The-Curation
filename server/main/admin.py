f8b708905258c8c06fe08a5280db23501276d103
Kayra <KayraAlat@gmail.com>
2021-05-16 13:05:43 +0100

    Added inline place images to admin

--- server/main/admin.py
+++ server/main/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from main.models import City, Place, Submission
+from main.models import City, Place, Submission, PlaceImage
+
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        PlaceImageInline,
+    ]
 
 
 admin.site.register(City)
-admin.site.register(Place)
 admin.site.register(Submission)
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(PlaceImage)
