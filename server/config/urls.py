3199e19439073ca24e347bbee9135713c0b1c89d
Kayra <KayraAlat@gmail.com>
2021-05-16 13:44:29 +0100

    Added static file url path

--- server/config/urls.py
+++ server/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from rest_framework import routers
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from main import views
 
@@ -33,3 +35,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
