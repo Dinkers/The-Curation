6da2450924e8ef1e33ac3f47b548b26b6a7bca0e
Robby <robby.cowell@gmail.com>
2021-05-21 12:56:34 -0700

    updating casing

---
+++ client/src/screens/Home/Home.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { getCities } from './homeSlice'
+import CitySelect from 'screens/Home/CitySelect'
+import FilterSelect from 'screens/Home/FilterSelect'
+
+function Home () {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getCities())
+  })
+
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="block">
+          <h3 className="title is-4">Find places in</h3>
+          <CitySelect />
+        </div>
+
+        <div className="block">
+          <h3 className="title is-4">Filters</h3>
+          <FilterSelect />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
