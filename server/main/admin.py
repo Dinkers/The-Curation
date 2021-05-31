c2c5cab9868d32b07d2a9217aa99d088944c08f9
Kayra <KayraAlat@gmail.com>
2021-05-31 20:34:02 +0100

    Updated initial migration and admin pages

--- server/main/admin.py
+++ server/main/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
 
-from main.models import City, Place, Submission, PlaceImage
+from main.models import City, Place, Submission, PlaceImage, PlaceOpeningHours, PlaceUSP, PlaceVitalInfo
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
 
 
+class PlaceOpeningHoursInline(admin.TabularInline):
+    model = PlaceOpeningHours
+
+
+class PlaceUSPInline(admin.TabularInline):
+    model = PlaceUSP
+
+
+class PlaceVitalInfoInline(admin.TabularInline):
+    model = PlaceVitalInfo
+
+
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         PlaceImageInline,
+        PlaceOpeningHoursInline,
+        PlaceUSPInline,
+        PlaceVitalInfoInline
     ]
 
 
