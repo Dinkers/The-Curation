aaa6a8e23ce80ff2433c82e2aa46b809d34194e2
Kayra <KayraAlat@gmail.com>
2021-06-08 20:30:26 +0100

    Filters returning empty array for city with no places

--- server/main/views.py
+++ server/main/views.py
@@ -62,7 +62,7 @@ def filters_list(request, city_id=None):
 
         places = city.places.all()
         if not places:
-            return Response([], status=status.HTTP_204_NO_CONTENT)
+            return Response([])
 
         usps = []
         vital_infos = []
