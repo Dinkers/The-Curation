765510199f3e4a92011e2d12aef0a5aebb91b61a
Robby <robby.cowell@gmail.com>
2021-06-12 07:45:42 -0700

    addingplace data to place screen

--- client/src/containers/Place/Place.js
+++ client/src/containers/Place/Place.js
@@ -3,25 +3,51 @@ import { useSelector, useDispatch } from 'react-redux'
 
 import { setCurrentScreen, setSelectedPlaceId } from 'containers/App/data/appSlice'
 import { getAppData } from 'containers/App/data/appSelectors'
+import { requestGetPlace, resetPlaceRequest } from 'containers/Place/data/placeSlice'
+import { getPlaceData } from 'containers/Place/data/placeSelectors'
+
+import Notification from 'components/Notification/Notification'
 
 const Place = () => {
   const dispatch = useDispatch()
   const appData = useSelector(getAppData)
+  const placeData = useSelector(getPlaceData)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (placeData.placeRequest === 'initial') {
+      dispatch(
+        requestGetPlace(appData.selectedPlaceId)
+      )
+    }
+  })
 
   const handleHomeClick = () => {
     dispatch(setCurrentScreen('Home'))
     dispatch(setSelectedPlaceId(null))
+    dispatch(resetPlaceRequest())
   }
 
   return (
     <>
-      <button className="button" onClick={ () => handleHomeClick() }>
-        Home
-      </button>
-      <h1>Place screen!!!</h1>
-      <p>Selected place Id: {appData.selectedPlaceId}</p>
+      { placeData.placeRequest === 'completed'
+      ? (
+        <div>
+          <button className="button" onClick={ () => handleHomeClick() }>
+            Home
+          </button>
+
+          <p>Name: { placeData.place.name }</p>
+          <p>Type: { placeData.place.place_type }</p>
+          <p>Website: { placeData.place.website }</p>
+          <p>Address: {placeData.place.address }</p>
+          <p>Email: { placeData.place.email_address }</p>
+          <p>Copy: {placeData.place.copy }</p>
+        </div>
+      ): (
+        <Notification 
+          message="Loading place data"
+        />
+      )}
     </>
   )
 }
