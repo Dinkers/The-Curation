2c88453fbcf4d71fea29e21f144dac39ba2d90f1
Robby <robby.cowell@gmail.com>
2021-07-01 19:26:34 -0700

    adding vital info and usps to place screen

--- client/src/containers/Place/Place.js
+++ client/src/containers/Place/Place.js
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from 'react-redux'
 
 import { setCurrentScreen, setSelectedPlaceId } from 'containers/App/data/appSlice'
 import { getAppData } from 'containers/App/data/appSelectors'
-import { requestGetPlace, resetPlaceRequest } from 'containers/Place/data/placeSlice'
-import { getPlaceData } from 'containers/Place/data/placeSelectors'
+import { requestGetPlace, resetPlaceData, getAllVitalInfos, getAllUsps } from 'containers/Place/data/placeSlice'
+import { getPlaceData, getVitalInfoData, getUspsData } from 'containers/Place/data/placeSelectors'
 
 import Notification from 'components/Notification/Notification'
 
@@ -13,8 +13,11 @@ import Slider from 'components/Slider/Slider'
 
 const Place = () => {
   const dispatch = useDispatch()
+
   const appData = useSelector(getAppData)
   const placeData = useSelector(getPlaceData)
+  const vitalInfoData = useSelector(getVitalInfoData)
+  const uspsData = useSelector(getUspsData)
 
   const contentStartRef = useRef(null)
 
@@ -26,10 +29,21 @@ const Place = () => {
     }
   })
 
+  useEffect(() => {
+    if (
+      placeData.placeRequest === 'completed' 
+      && vitalInfoData.vitalInfoRequest === 'initial'
+    ) {
+      dispatch(getAllVitalInfos(placeData.place['vital_infos']))
+      dispatch(getAllUsps(placeData.place['usps']))
+      // TODO: dispatch image request here
+    }
+  }, [placeData, vitalInfoData, dispatch])
+
   const handleHomeClick = () => {
     dispatch(setCurrentScreen('Home'))
     dispatch(setSelectedPlaceId(null))
-    dispatch(resetPlaceRequest())
+    dispatch(resetPlaceData())
   }
 
   const scrollToContentStart = () => {
@@ -49,12 +63,11 @@ const Place = () => {
 
           <section className="section" ref={contentStartRef}>
             <div className="container">
-              {/* 
-                <button className="button" onClick={ () => handleHomeClick() }>
-                  Home
-                </button> 
-              */}
-
+              
+              {/* <button className="button" onClick={ () => handleHomeClick() }>
+                Home
+              </button>  */}
+             
               <div className="block">
                 <h1 className="title">{ placeData.place.name }</h1>
                 <h2 className="subtitle">Open Now</h2>
@@ -65,12 +78,41 @@ const Place = () => {
               </div>
 
               <div className="block">
-                <p>Name: { placeData.place.name }</p>
-                <p>Type: { placeData.place.place_type }</p>
-                <p>Website: { placeData.place.website }</p>
-                <p>Address: {placeData.place.address }</p>
-                <p>Email: { placeData.place.email_address }</p>
-                <p>Copy: { placeData.place.copy }</p>
+                <h2 className="subtitle">Vital info</h2>
+                <div className="content">
+                  {
+                    vitalInfoData.vitalInfoRequest === 'completed'
+                      ? (
+                        <ul>
+                          { vitalInfoData.vitalInfos.map((vitalInfo) => <li>{ vitalInfo['vital_info']}</li>) }
+                        </ul>
+                      )
+                      : ( <p>Loading</p> )
+                  }
+                </div>
+              </div>
+
+              <div className="block">
+                <h2 className="subtitle">Reasons to visit</h2>
+                <div className="content">
+                  {
+                    uspsData.uspsRequest === 'completed'
+                      ? (
+                        <ul>
+                          { uspsData.usps.map((usp) => <li>{usp.usp}</li>) }
+                        </ul>
+                      ) 
+                      : ( <p>Loading</p> )
+                  }
+                </div>
+              </div>
+
+              <div className="block">
+              <div className="content">
+                  <p>Find: {placeData.place.address }</p>
+                  <p>Write: { placeData.place.email_address }</p>
+                  <p>Website: { placeData.place.website }</p>
+                </div>
               </div>
             </div>
           </section>
--- client/src/containers/Place/data/placeSelectors.js
+++ client/src/containers/Place/data/placeSelectors.js
@@ -1,3 +1,4 @@
+import { createSlice } from '@reduxjs/toolkit'
 import { createSelector } from 'reselect' 
 
 export const getPlace = (state) => state.place.place
@@ -8,9 +9,35 @@ export const getPlaceData = createSelector(
   getPlace,
   getPlaceId,
   getPlaceRequest,
-  (place, placeId, placeRequest) => ({
+  (place, placeId, placeRequest, getPlaceVitalInfos, vitalInfoRequest) => ({
     place,
     placeId,
-    placeRequest
+    placeRequest,
+    getPlaceVitalInfos,
+    vitalInfoRequest
+  })
+)
+
+export const getVitalInfos = (state) => state.place.vitalInfos
+export const getVitalInfoRequest = (state) => state.place.vitalInfoRequest
+
+export const getVitalInfoData = createSelector(
+  getVitalInfos,
+  getVitalInfoRequest,
+  (vitalInfos, vitalInfoRequest) => ({
+    vitalInfos,
+    vitalInfoRequest
+  })
+)
+
+export const getUsps = (state) => state.place.usps
+export const getUspsRequest = (state) => state.place.uspsRequest
+
+export const getUspsData = createSelector(
+  getUsps,
+  getUspsRequest,
+  (usps, uspsRequest) => ({
+    usps,
+    uspsRequest
   })
 )
